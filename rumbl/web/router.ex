bc2399be4e3697593cb685314c9358dcec97cad7
Marcel Fahle <m.fahle@gmail.com>
2017-02-28 11:47:45 +0100

    Rumbl: adds basic user model and creation

--- rumbl/web/router.ex
+++ rumbl/web/router.ex
@@ -17,7 +17,9 @@ defmodule Rumbl.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
   end
+
 
   # Other scopes may use custom stacks.
   # scope "/api", Rumbl do
