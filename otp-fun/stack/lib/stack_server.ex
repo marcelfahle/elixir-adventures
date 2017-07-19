9466240ef33d7e9f599335f3b5e4b8e99d1d5e8d
Marcel Fahle <m.fahle@gmail.com>
2017-07-19 10:50:25 +0300

    updates a few more examples

--- otp-fun/stack/lib/stack_server.ex
+++ otp-fun/stack/lib/stack_server.ex
@@ -1,6 +1,27 @@
 defmodule StackServer do
   use GenServer
 
+  # api
+
+  def start_link(state) do
+    GenServer.start_link(__MODULE__, state, name: :stack_server)
+  end
+
+  def pop do
+    GenServer.call(:stack_server, :pop)
+  end
+
+  def push(val) do
+    GenServer.cast(:stack_server, {:push, val})
+  end
+
+  def list do
+    GenServer.call(:stack_server, :list)
+  end
+
+
+  # Callbacks
+
   def handle_call(:pop, _from, state) do
     {:reply, Enum.at(state, 0), Enum.drop(state, 1)}
   end
---
+++ otp-fun/thy_supervisor/lib/thy_worker.ex
@@ -0,0 +1,16 @@
+defmodule ThyWorker do
+
+  def start_link do
+    spawn(fn -> loop end)
+  end
+
+  def loop do
+    receive do
+      :stop -> :ok
+
+      msg ->
+        IO.inspect msg
+        loop
+    end
+  end
+end
